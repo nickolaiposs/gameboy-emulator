ef2f4a7459089e1007cd43bc00483c398418ff6e
Nickolai Possokhov <npossokhov@gmail.com>
2022-12-23 04:10:31 -0500

    started adding file loading functionality to the load rom button

--- www/main.js
+++ www/main.js
@@ -17,4 +17,15 @@ document.querySelector("#controlexit").onclick = toggle_config;
 document.querySelector("#controlconfig").onclick = toggle_config;
 document.querySelector("#configbutton").onclick = toggle_config;
 
+//add rom filebrowser
+const rom_button = document.querySelector("#loadrom");
+rom_button.onclick = file_browse;
+
+function file_browse() {
+    var inputElement = document.createElement("input");
+    inputElement.type = "file";
+    inputElement.accept = ".rom";
+    inputElement.click();
+}
+
 render(ctx, canvas_width, canvas_height);
