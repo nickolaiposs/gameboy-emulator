9e0b658d54fc965ad3c099d33405968a8cf99ac9
Nickolai Possokhov <npossokhov@gmail.com>
2023-05-11 15:14:02 -0400

    forgot to remote commands.rs from .gitignore

---
+++ src/lib/commands.rs
@@ -0,0 +1,176 @@
+// From gameboy CPU manual
+
+// load
+pub fn LD() {
+    
+}
+
+pub fn PUSH() {
+
+}
+
+pub fn POP() {
+
+}
+
+// ALU
+pub fn ADD() {
+
+}
+
+pub fn ADC() {
+
+}
+
+pub fn SUB() {
+
+}
+
+pub fn SBC() {
+
+}
+
+pub fn AND() {
+
+}
+
+pub fn XOR() {
+
+}
+
+pub fn OR() {
+
+}
+
+pub fn DEC() {
+
+}
+
+
+// MISC
+pub fn SWAP() {
+
+}
+
+pub fn DAA() {
+
+}
+
+pub fn CPL() {
+
+}
+
+pub fn CCF() {
+
+}
+
+pub fn SCF() {
+
+}
+
+pub fn NOP() {
+
+}
+
+pub fn HALT() {
+
+}
+
+pub fn STOP() {
+
+}
+
+pub fn DI() {
+
+}
+
+pub fn EI() {
+
+}
+
+// rotates and shifts
+pub fn RLCA() {
+
+}
+
+pub fn RLA() {
+
+}
+
+pub fn RRCA() {
+
+}
+
+pub fn RRA() {
+
+}
+
+pub fn RLC() {
+
+}
+
+pub fn RL() {
+
+}
+
+pub fn RRC() {
+
+}
+
+pub fn RR() {
+
+}
+
+pub fn SLA() {
+
+}
+
+pub fn SRA() {
+
+}
+
+pub fn SRL() {
+
+}
+
+// bit opcodes
+pub fn BIT() {
+
+}
+
+pub fn SET() {
+
+}
+
+pub fn RES() {
+
+}
+
+// jumps
+pub fn JP() {
+
+}
+
+pub fn JR() {
+
+}
+
+// call
+pub fn CALL() {
+
+}
+
+// restart
+pub fn RST() {
+
+}
+
+// returns
+pub fn RET() {
+
+}
+
+pub fn RETI() {
+    
+}
+
