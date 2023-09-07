a6cebbbfe0830b782ea6ebcd8abd0bf88a544adb
Nickolai Possokhov <npossokhov@gmail.com>
2023-09-07 14:46:53 -0400

    added lcd planning comment

--- src/lib/lcd.rs
+++ src/lib/lcd.rs
@@ -3,6 +3,10 @@ pub struct LCD {
 }
 
 impl LCD {
+    /*
+    the screen is a one dimensional array that is read row by row, left to right, 
+    up to down, and each pixel is four entries on the array representing RGBA values
+    */
     pub fn new(screen_width: u32, screen_height: u32) -> Self {
         Self {
             screen: vec![255; (screen_width as usize) * (screen_height as usize) * 4]
